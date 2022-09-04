7beb21b1ffc4737080e2dfecc643daa5ed3eb40d
Matt Carrier <icecreammatt@gmail.com>
2022-09-03 18:23:32 -0700

    feat: add example footer story

---
+++ src/stories/Footer.stories.js
@@ -0,0 +1,51 @@
+import Footer from './Footer.svelte';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes
+export default {
+    title: 'Example/Footer',
+    component: Footer,
+    // argTypes: {
+    //     backgroundColor: { control: 'color' },
+    //     label: { control: 'text' },
+    //     onClick: { action: 'onClick' },
+    //     primary: { control: 'boolean' },
+    //     size: {
+    //         control: { type: 'select' },
+    //         options: ['small', 'medium', 'large'],
+    //     },
+    // },
+};
+
+// More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args
+const Template = (args) => ({
+    Component: Footer,
+    props: args,
+    // on: {
+    //     click: args.onClick,
+    // },
+});
+
+// More on args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Primary = Template.bind({});
+Primary.args = {
+    // primary: true,
+    // label: 'Footer',
+};
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//     label: 'Footer',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//     size: 'large',
+//     label: 'Footer',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//     size: 'small',
+//     label: 'Footer',
+// };
---
+++ src/stories/Footer.svelte
@@ -0,0 +1,43 @@
+<footer>
+	<h5>Copyright 2022</h5>
+	<nav>
+		<ul>
+			<li>Github</li>
+			<li>Linkedin</li>
+			<li>Stackoverflow</li>
+			<li>Twitter</li>
+		</ul>
+	</nav>
+</footer>
+
+<style>
+	footer {
+		/* padding: 1rem; */
+		background: lightskyblue;
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	nav {
+		align-items: center;
+	}
+
+	ul {
+		margin: 0;
+		list-style-type: none;
+		list-style: disc;
+		display: flex;
+		gap: 1rem;
+		background-color: green;
+	}
+
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+	a:hover {
+		background-color: red;
+	}
+</style>
