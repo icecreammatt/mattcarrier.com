02f525c2cfc75af590bb478eb1099f52c8beeaa0
Matt Carrier <icecreammatt@gmail.com>
2022-09-05 13:18:02 -0700

    style: styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,14 @@
 	<slot />
 </main>
 <Footer />
+
+<style>
+	main {
+		display: flex;
+		min-width: 450px;
+		align-content: center;
+		justify-content: center;
+		padding-bottom: 2rem;
+		padding-top: 2rem;
+	}
+</style>
