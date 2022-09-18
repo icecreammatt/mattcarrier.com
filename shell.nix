35ea96fa0f97ad7bf9f6f0d171d026ee50490468
Matt Carrier <icecreammatt@gmail.com>
2022-09-17 23:31:42 -0700

    fix: add node scripts

--- shell.nix
+++ shell.nix
@@ -4,15 +4,30 @@
 
 with (import <nixpkgs> {});
 
-stdenv.mkDerivation rec {
+let
+
+  install = pkgs.writeScriptBin "nodeinstall" ''
+    exec ${pkgs.nodejs-18_x}/bin/npm install
+  '';
+
+  build = pkgs.writeScriptBin "nodebuild" ''
+    exec ${pkgs.nodejs-18_x}/bin/npm run build
+  '';
+
+in mkShell {
+# stdenv.mkDerivation rec {
   name = "node-environment";
+
   buildInputs = [
       nodejs-18_x
       fish
       nodePackages.eslint
       nodePackages.prettier
+      install
+      build
       nodePackages.svelte-language-server
   ];
+
   shellHook = ''
     echo "Using node: ${nodejs-18_x.version}"
     fish
