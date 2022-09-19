25cf3e4fc9daa78e43e5fb97b4434d982a7f07d2
Matt Carrier <icecreammatt@gmail.com>
2022-09-19 06:59:56 +0000

    feat: add header to storybook

---
+++ src/stories/Header.stories.js
@@ -0,0 +1,51 @@
+import Header from './Header.svelte';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes
+export default {
+	title: 'Header',
+	component: Header
+	// argTypes: {
+	//     backgroundColor: { control: 'color' },
+	//     label: { control: 'text' },
+	//     onClick: { action: 'onClick' },
+	//     primary: { control: 'boolean' },
+	//     size: {
+	//         control: { type: 'select' },
+	//         options: ['small', 'medium', 'large'],
+	//     },
+	// },
+};
+
+// More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args
+const Template = (args) => ({
+	Component: Header,
+	props: args
+	// on: {
+	//     click: args.onClick,
+	// },
+});
+
+// More on args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Primary = Template.bind({});
+Primary.args = {
+	// primary: true,
+	// label: 'Header',
+};
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//     label: 'Header',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//     size: 'large',
+//     label: 'Header',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//     size: 'small',
+//     label: 'Header',
+// };
