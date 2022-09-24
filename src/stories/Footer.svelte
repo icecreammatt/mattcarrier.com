64972fb6fe7fa03c3bbc17083c66c6d373d120ea
Matt Carrier <icecreammatt@gmail.com>
2022-09-24 23:29:50 +0000

    fix: update footer links

--- src/stories/Footer.svelte
+++ src/stories/Footer.svelte
@@ -1,45 +1,51 @@
+<script>
+import Button from './Button.svelte';
+
+</script>
+
 <footer>
 	<nav>
 		<ul>
-			<li>Copyright 2022</li>
-			<li>Github</li>
-			<li>Linkedin</li>
-			<li>Stackoverflow</li>
-			<li>Twitter</li>
+            <li><span>Copyright 2022</li>
+            <li><Button label="Github" url="https://www.github.com/icecreammatt/" /></li>
+            <li><Button label="Linkedin" url="https://www.linkedin.com/in/carriermatt/" /></li>
+            <li><Button label="Stack Overflow" url="https://stackoverflow.com/users/873559/matt-carrier" /></li>
 		</ul>
 	</nav>
 </footer>
 
 <style>
-	/* h5 {
-        margin-left: 2rem;
-    } */
-
 	footer {
-		/* padding: 1rem; */
+		align-self: flex-end;
 		background: #c8c8c8;
 		display: flex;
-		justify-content: space-between;
-		padding-left: 2rem;
-		padding-right: 2rem;
-		align-self: flex-end;
+        background-color: green;
 	}
 
 	nav {
-		display: flex;
 		align-items: center;
-		/* background-color: red; */
+		align-self: flex-end;
+		background-color: red;
+		display: flex;
+        justify-content: center;
 	}
 
 	ul {
 		display: flex;
 		justify-content: space-around;
 		list-style-type: none;
-		gap: 2rem;
 	}
 
-	/* a {
-        text-decoration: none;
-        color: inherit;
-    } */
+    /* li { */
+    /*     align-items: flex-end; */
+    /*     justify-content: center; */
+    /* } */
+
+    li:first-child {
+		padding-left: 1.0rem;
+		padding-right: 1.0rem;
+        display: flex;
+        padding: 0.5rem;
+    }
+
 </style>
