fca3d5717e79a2953fab742ac9682b29057fe107
Matt Carrier <icecreammatt@gmail.com>
2022-09-25 07:11:53 +0000

    feat: header button and styling

--- src/stories/Header.svelte
+++ src/stories/Header.svelte
@@ -1,21 +1,31 @@
+<script>
+	import Button from './Button.svelte';
+
+	// background: #c8c8c8;
+	// background-repeat: no-repeat;
+	// border-bottom: 1px solid #c8c8c8;
+</script>
+
 <header>
-	<a href="/">Home</a>
+	<Button label="Home" url="/" color="#000" background="rgba(255,255,255, 0.5)" />
 	<nav>
 		<ul>
-			<li><a href="/post">Blog</a></li>
-			<li><a href="/resume">Resume</a></li>
+			<li><Button label="Blog" url="/post" color="#000" background="rgba(255,255,255, 0.5)" /></li>
+			<li>
+				<Button label="Resume" url="/resume" color="#000" background="rgba(255,255,255, 0.5)" />
+			</li>
 		</ul>
 	</nav>
 </header>
 
 <style>
 	header {
-		padding: 1rem;
-		background: #c8c8c8;
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
-		border-bottom: 1px solid #c8c8c8;
+		background-image: url('../../images/banner.jpeg');
+		box-shadow: 0px -30px 20px -10px rgba(223, 190, 159, 1) inset;
+		min-height: 230px;
 	}
 
 	ul {
