4006e97465ceb9c0773aaac49114c4360bfc334f
Matt Carrier <icecreammatt@gmail.com>
2022-09-24 23:01:45 -0700

    fix: filter out resume post from posts page

--- src/routes/post/+page.ts
+++ src/routes/post/+page.ts
@@ -1,6 +1,9 @@
 export const load = async ({ fetch }) => {
 	const response = await fetch(`/api/posts`);
-	const posts = await response.json();
+	const allPosts = await response.json();
+
+	// Filter out resume post but keep it so links don't break
+	const posts = allPosts.filter((post) => post.path !== '/post/resume');
 
 	return {
 		posts
