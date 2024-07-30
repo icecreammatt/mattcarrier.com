d521dced04a0c730ab44d8ab77c100f97e9c289b
Matt Carrier <icecreammatt@gmail.com>
2024-07-29 21:21:50 -0700

    lint: update flake linting

--- flake.nix
+++ flake.nix
@@ -14,68 +14,76 @@
     utils.url = "github:numtide/flake-utils";
 
     vienna = {
-      url = github:icecreammatt/vienna/mattcarrier-2022;
+      url = "github:icecreammatt/vienna/mattcarrier-2022";
       flake = false;
     };
-
   };
 
-  outputs = { self, nixpkgs, vienna, ... }@inputs: inputs.utils.lib.eachSystem [
-    # Add the system/architecture you would like to support here. Note that not
-    # all packages in the official nixpkgs support all platforms.
-    "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
-  ] (system: let  pkgs = import nixpkgs { inherit system; };
-  in {
+  outputs = {
+    self,
+    nixpkgs,
+    vienna,
+    ...
+  } @ inputs:
+    inputs.utils.lib.eachSystem [
+      # Add the system/architecture you would like to support here. Note that not
+      # all packages in the official nixpkgs support all platforms.
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ] (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      # This block here is used when running `nix develop`
+      devShells.default = pkgs.mkShell rec {
+        # Update the name to something that suites your project.
+        name = "website";
 
-    # This block here is used when running `nix develop`
-    devShells.default = pkgs.mkShell rec {
-      # Update the name to something that suites your project.
-      name = "website";
+        packages = with pkgs; [hugo];
 
-      packages = with pkgs; [ hugo ];
+        buildPhase = "
+          mkdir themes
+          ln -sf ${vienna} themes/vienna
+          make build
+        ";
 
-      buildPhase = "
-        mkdir themes
-        ln -sf ${vienna} themes/vienna
-        make build
-      ";
+        # Setting up the environment variables you need during development.
 
-      # Setting up the environment variables you need during development.
+        # Todo figure out why I can't use clang on Asahi but can on Darwin
+        # Use "clang++" for most systems but OSX Asahi requires g++ for some reason or a runtime error occurs
+        shellHook = let
+          # This is for an icon that is used below for the command line input below
+          icon = "f121";
+        in ''
+          export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+        '';
+      };
 
-      # Todo figure out why I can't use clang on Asahi but can on Darwin
-      # Use "clang++" for most systems but OSX Asahi requires g++ for some reason or a runtime error occurs
-      shellHook = let
-        # This is for an icon that is used below for the command line input below
-        icon = "f121";
-      in ''
-        export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-      '';
-    };
+      # This is used when running `nix build`
+      packages.default =
+        pkgs.stdenv.mkDerivation {
+          name = "website";
+          version = "0.0.1";
 
-    # This is used when running `nix build`
-    packages.default = pkgs.stdenv.mkDerivation rec {
-      name = "website";
-      version = "0.0.1";
+          src = self;
 
-      src = self;
+          buildInputs = [pkgs.hugo];
 
-      buildInputs = [ pkgs.hugo ];
-
-      buildPhase = "
+          buildPhase = "
         mkdir -p themes
         ln -sf ${vienna} themes/vienna
         make build
       ";
 
-      installPhase = ''
-        cp -r public $out
-      '';
+          installPhase = ''
+            cp -r public $out
+          '';
 
-      meta = with inputs.utils.lib; {
-        homepage = "github.com/icecreammatt/mattcarrier.com";
-        description = "Website";
-      };
-    };
-  });
-
+          meta = {
+            homepage = "github.com/icecreammatt/mattcarrier.com";
+            description = "Website";
+          };
+        };
+    });
 }
